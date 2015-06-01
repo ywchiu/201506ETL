7fc4900817bf281f9e39ba26a4ac7788949c2201
qoo <david@largitdata.com>
2015-06-01 10:41:29 +0800

    add course1

---
+++ call.py
@@ -0,0 +1,2 @@
+import main
+print "this is call"
---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+
+# This is a single line comment
+def main():
+    print 'Hello World'
+print "this is main"
+	
+if __name__ == '__main__':
+    main()
